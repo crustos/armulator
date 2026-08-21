bf531a8d1c7c5057c094fe69f37bc2851f6ae4ca
brent hartshorn <brenthartshorn@proton.me>
2026-08-21 13:17:08 -0700

    Jetson Nano and Raspi GPIO emulation - part 14

--- armulator/peripherals/__init__.py
+++ armulator/peripherals/__init__.py
@@ -6,6 +6,7 @@ from armulator.peripherals.serial_bus import (
     Bcm2835I2c, Bcm2835Spi, I2cSlaveDevice, SpiLoopback, SpiSlaveDevice,
 )
 from armulator.peripherals.spi_slave import Bcm2835SpiSlave, address_octet
+from armulator.peripherals.spi_tegra import Tegra210Spi
 from armulator.peripherals.uart_pl011 import BcmSystemTimer, Pl011Uart
 
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
     'Pl011Uart', 'BcmSystemTimer', 'Gic400',
     'Bcm2835Spi', 'Bcm2835I2c', 'SpiSlaveDevice', 'SpiLoopback',
     'I2cSlaveDevice', 'Bcm2835SpiSlave', 'address_octet',
+    'Tegra210Spi',
 ]
